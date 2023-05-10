2eeb5b2792c2729ed5e9838244b6376fbfcbca53
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-05-10 14:04:05 -0600

    Last commit

--- lib/presentation/screens/discover/discover_screen.dart
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final discoverProvider = context.watch<DiscoverProvider>();
+
+    final otroProvider = Provider.of<DiscoverProvider>(context);
     return const Scaffold(
-      body: Center(
-        child: Text('Discover Screen')),
+      body: Center(child: Text('Discover Screen')),
     );
   }
 }
